1f34910cfb3dc812feb375224e290dcfc17ae546
imsdolta <imsdolta@gmail.com>
2020-09-04 10:08:22 +0530

    :tada: functional now

---
+++ index.js
@@ -0,0 +1,81 @@
+let characterAmountRange = document.getElementById('characterAmountRange')
+let characterAmountNumber = document.getElementById('characterAmountNumber')
+const includeUppercaseElement = document.getElementById('includeUppercase')
+const includeNumbersElement = document.getElementById('includeNumbers')
+const includeSymbolsElement = document.getElementById('includeSymbols')
+
+const form = document.getElementById('passwordGeneratorForm')
+const passwordDisplay = document.getElementById('passwordDisplay')
+
+
+
+
+function getPassword(characterAmount, includeUppercase, includeNumbers, includeSymbols) {
+
+    var numChars = "0123456789"
+    var upperChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    var lowerChars = "abcdefghijklmnopqrstuvwxyz"
+    var symbolChars = "?></\!@#$%^&*"
+
+    var allChars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz?></\!@#$%^&*"
+
+
+    var chars = ""
+    if (includeUppercase && includeNumbers && includeSymbols) {
+        chars = lowerChars + upperChars + numChars + symbolChars
+    } else if (includeSymbols && includeNumbers) {
+        chars = symbolChars + numChars + lowerChars
+    } else if (includeSymbols && includeUppercase) {
+        chars = symbolChars + upperChars + lowerChars
+    } else if (includeNumbers && includeUppercase) {
+        chars = numChars + upperChars + lowerChars
+    } else if (includeUppercase) {
+        chars = lowerChars + upperChars
+    } else if (includeNumbers) {
+        chars = lowerChars + numChars
+    } else if (includeSymbols) {
+        chars = symbolChars
+    } else {
+        chars = lowerChars
+    }
+    return generate(characterAmount, chars)
+
+}
+
+function generate(characterAmount, chars) {
+    return Array(parseInt(characterAmount)).fill(chars).map((x) => {
+        return x[Math.floor(Math.random() * x.length)]
+    }).join('')
+
+}
+
+characterAmountNumber.addEventListener('input', sync)
+characterAmountRange.addEventListener('input', sync)
+
+
+function sync(e) {
+    const val = e.target.value;
+    characterAmountRange.value = val
+    characterAmountNumber.value = val
+}
+
+form.addEventListener('submit', e => {
+    e.preventDefault()
+    const characterAmount = characterAmountNumber.value
+    const includeUppercase = includeUppercaseElement.checked
+    const includeNumbers = includeNumbersElement.checked
+    const includeSymbols = includeSymbolsElement.checked
+    const password = getPassword(characterAmount, includeUppercase, includeNumbers, includeSymbols)
+    passwordDisplay.innerText = password
+
+})
+
+const copyToClipboard = str => {
+    passwordDisplay.setAttribute('readonly', '')
+    passwordDisplay.style.position = 'absolute'
+    passwordDisplay.style.left = '-999px'
+    document.body.appendChild(passwordDisplay)
+    passwordDisplay.select();
+    document.execCommand('copy')
+    document.body.removeChild(passwordDisplays)
+}
